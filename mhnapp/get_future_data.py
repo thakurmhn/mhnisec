1a5802f2ad2af869ee23d0684f4a41fb8e034eb5
thakurmhn <mohan_thakoor@yahoo.co.in>
2023-06-24 16:29:34 +0530

    new updates

--- mhnapp/get_future_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-scrip = pd.read_csv("Data/FONSEScripMaster.txt")
-scrip = scrip[['Token', 'InstrumentName', 'ShortName', 'Series','ExpiryDate', 'StrikePrice', 'OptionType', 'ExchangeCode']]
-print(scrip.head())
